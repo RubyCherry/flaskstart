b7d6be12ee0e144b9469d8a37e7fb26c49f20999
Артем Набиев <dlya.gitpoda@gmail.com>
2022-05-23 06:37:42 +0000

    upload

--- app/__init__.py
+++ app/__init__.py
@@ -11,16 +11,24 @@ from flask import  session, request
 # from app import routes
 # from app import special
 
+UPLOAD_FOLDER = os.path.join(os.getcwd(),'uploads')
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+
 def create_app(test_config=None):
     app = Flask(__name__, 
                 instance_relative_config=True, 
-                template_folder='bulma')
-    
+                template_folder='bulma',)
+                
+    print('NAME',app.name)
+    app.debug=True;
+
     app.config.from_mapping(
         SECRET_KEY='123456789',
         DATABASE=os.path.join(app.instance_path, 'database.db'),
         COUNTRY='Russia',
-        DEBUG_TB_INTERCEPT_REDIRECTS = False
+        DEBUG_TB_INTERCEPT_REDIRECTS = False,
+         UPLOAD_FOLDER=UPLOAD_FOLDER        
     )
     
     print("DB? ",os.path.join(app.instance_path, 'database.db'))
@@ -54,3 +62,4 @@ def create_app(test_config=None):
         return '<body>Тестовый маршрут!</body>'
     
     return app
+    
